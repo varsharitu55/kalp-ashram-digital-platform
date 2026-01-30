e26fe52b42e71f13f4e6ae958be94b0d321da840
Varsha <varsharitu55@gmail.com>
2026-01-30 14:42:30 -0500

    Added securityConfig class for security

---
+++ user/src/main/java/com/kalpashram/user/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.kalpashram.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+    {
+        http
+                .authorizeHttpRequests(auth-> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
