1f4ddab52a593ad551cdb88a1a972fdeac17e1b7
Varsha <varsharitu55@gmail.com>
2026-02-05 18:39:29 -0500

    new constant added

--- user/src/main/java/com/kalpashram/user/constants/UserConstants.java
+++ user/src/main/java/com/kalpashram/user/constants/UserConstants.java
@@ -5,9 +5,11 @@ public final class UserConstants {
     }
 
     public static final String STATUS_201 = "201";
-    public static final String MESSAGE_201 = "User created successfully";
+    public static final String MESSAGE_201 = "User created successfully ";
     public static final String STATUS_200 = "200";
     public static final String MESSAGE_200 = "Request processed successfully";
     public static final String  STATUS_500 = "500";
     public static final String  MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
+    public static final String  STATUS_404 = "404";
+    public static final String  MESSAGE_404 = "Resource not found";
 }
